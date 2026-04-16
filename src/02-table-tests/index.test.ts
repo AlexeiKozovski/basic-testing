3c6cfbc31e00aa5e882bc62dcb2a56892b0c8794
Alexey Kozovski <alexey.kozovski@idfinance.com>
2026-04-17 02:33:41 +0300

    refactor: delete unnecessary tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -2,8 +2,6 @@ import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 5, b: 3, action: Action.Subtract, expected: 2 },
   { a: 2, b: 2, action: Action.Multiply, expected: 4 },
   { a: 4, b: 2, action: Action.Divide, expected: 2 },
